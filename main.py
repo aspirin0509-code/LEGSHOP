4de2feb6971c813ed0fbb5bfe536ff8cec9636a8
aspirin0509-code <aspirin0509@gmail.com>
2026-05-14 03:10:04 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1633,10 +1633,7 @@ def run_bot():
     app.add_handler(MessageHandler(filters.Document.PDF, handle_document))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_phone_lookup))
     app.add_handler(CallbackQueryHandler(callback_query))
-    app.run_polling(
-        drop_pending_updates=True,
-        stop_signals=None
-    )
+   
 #-------------START---------    
 if __name__ == "__main__":
     Thread(target=run_bot, daemon=True).start()
