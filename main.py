ea7a9a64cff81697bca9e6c085605fc44d632fe6
aspirin0509-code <aspirin0509@gmail.com>
2026-05-14 00:08:29 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1655,4 +1655,5 @@ def main():
          print("[WEB] Flask started")
          app.run_polling(drop_pending_updates=True)
 if __name__ == "__main__":
-    main()
+    # main()
+    flask_app.run(host="0.0.0.0", port=PORT)
