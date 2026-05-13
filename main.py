d4a6981d2c27d5d2578fec01748bf3513895caea
aspirin0509-code <aspirin0509@gmail.com>
2026-05-14 01:49:38 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1601,9 +1601,9 @@ flask_app = Flask(__name__)
 def home():
     return "LEGSHOP WORKING"
 
-    if __name__ == "__main__":
-        flask_app.run(
-            host="0.0.0.0",
-            port=int(os.environ.get("PORT", 8080))
-        )
+if __name__ == "__main__":
+    flask_app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080))
+    )
     
