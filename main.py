bbbc48767cb0aed54661af06325c1649bd9c5697
aspirin0509-code <aspirin0509@gmail.com>
2026-05-14 00:41:59 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1605,7 +1605,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     flask_app.run(
         host="0.0.0.0",
-        port=port
+        port=port,
         debug=False
     )
     
