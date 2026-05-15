275c56f5232513205d4deb271990320a0ad1ec07
aspirin0509-code <aspirin0509@gmail.com>
2026-05-15 17:58:51 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1693,8 +1693,7 @@ async def main_bot():
     bot_app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_phone_lookup))
     bot_app.add_handler(CallbackQueryHandler(callback_query))
 
-    await bot_app.initialize()
-    await bot_app.updater.start_polling(drop_pending_updates=True)
+    await bot_app.run_polling(drop_pending_updates=True)
     print("[BOT] Started polling")
     await asyncio.Event().wait()
 
